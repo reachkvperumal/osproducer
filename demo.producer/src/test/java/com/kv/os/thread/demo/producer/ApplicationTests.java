ef117f3629732957569351845409af454284ec25
KV Perumal <kvperumal@KVs-MacBook-Pro.local>
2023-12-20 00:47:36 -0500

    Initial Commit

---
+++ demo.producer/src/test/java/com/kv/os/thread/demo/producer/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.kv.os.thread.demo.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println(Math.pow(2,12));
+	}
+
+}
